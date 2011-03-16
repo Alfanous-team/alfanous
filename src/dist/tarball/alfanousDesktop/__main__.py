f7b7a2668508ad99d03599c30e4f4bb09358e12d
Assem Chelli <assem.ch@gmail.com>
2011-03-16 14:54:23 +0000

    SIS + Tarball
    Former-commit-id: 09a0d64d6411268e743801ada67dec08fc9d95ee

---
+++ src/dist/tarball/alfanousDesktop/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*- 
+import sys
+
+sys.path.append("/usr/lib/alfanousDesktop/alfanous.egg")
+sys.path.append("/usr/lib/alfanousDesktop/whoosh.egg")
+
+from Gui import main
+
+
+main()
