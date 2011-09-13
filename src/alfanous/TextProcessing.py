ac652aed117e46164c65736b8f1b5703ee876fa1
Assem Chelli <assem.ch@gmail.com>
2011-09-13 16:03:09 +0000

    Adding AGPL licence header to all source files
    Former-commit-id: 2d56e256d7987f7733e607867ade8759ccb27e23

--- src/alfanous/TextProcessing.py
+++ src/alfanous/TextProcessing.py
@@ -1,18 +1,21 @@
 # coding: utf-8
-#    Copyright (C) 2009-2010 Assem Chelli <assem.ch@gmail.com>
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+##     Copyright (C) 2009-2012 Assem Chelli <assem.ch [at] gmail.com>
+
+##     This program is free software: you can redistribute it and/or modify
+##     it under the terms of the GNU Affero General Public License as published by
+##     the Free Software Foundation, either version 3 of the License, or
+##     (at your option) any later version.
+
+##     This program is distributed in the hope that it will be useful,
+##     but WITHOUT ANY WARRANTY; without even the implied warranty of
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     GNU Affero General Public License for more details.
+
+##     You should have received a copy of the GNU Affero General Public License
+##     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 '''
 it contains the linguistic analysers of Quran and Arabic...etc.
@@ -20,7 +23,7 @@ it contains the linguistic analysers of Quran and Arabic...etc.
 
 @author: Assem Chelli
 @contact: assem.ch [at] gmail.com
-@license: GPL
+@license: AGPL
 
 
 @todo: pronoun Reference  " == لا إله إلا الله " لا إله إلا هو
