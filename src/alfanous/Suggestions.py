2d56e256d7987f7733e607867ade8759ccb27e23
Assem Chelli <assem.ch@gmail.com>
2011-09-13 16:03:09 +0000

    Adding AGPL licence header to all source files

--- src/alfanous/Suggestions.py
+++ src/alfanous/Suggestions.py
@@ -1,3 +1,21 @@
+# coding: utf-8
+
+##     Copyright (C) 2009-2012 Assem Chelli <assem.ch [at] gmail.com>
+
+##     This program is free software: you can redistribute it and/or modify
+##     it under the terms of the GNU Affero General Public License as published by
+##     the Free Software Foundation, either version 3 of the License, or
+##     (at your option) any later version.
+
+##     This program is distributed in the hope that it will be useful,
+##     but WITHOUT ANY WARRANTY; without even the implied warranty of
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     GNU Affero General Public License for more details.
+
+##     You should have received a copy of the GNU Affero General Public License
+##     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 '''
 Created on 22 avr. 2010
 
@@ -5,8 +23,8 @@ it contains  suggestions systems
 
 @author: Assem Chelli
 @contact: assem.ch [at] gmail.com
-@license: GPL
-@organization: Waiting support 
+@license: AGPL
+
 
 
 
--- src/alfanous/main.py
+++ src/alfanous/main.py
@@ -1,26 +1,26 @@
 # coding: utf-8
 
 
-#    Copyright (C) 2009-2010 Assem Chelli <assem.ch@gmail.com>
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+##     Copyright (C) 2009-2012 Assem Chelli <assem.ch [at] gmail.com>
+
+##     This program is free software: you can redistribute it and/or modify
+##     it under the terms of the GNU Affero General Public License as published by
+##     the Free Software Foundation, either version 3 of the License, or
+##     (at your option) any later version.
+
+##     This program is distributed in the hope that it will be useful,
+##     but WITHOUT ANY WARRANTY; without even the implied warranty of
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     GNU Affero General Public License for more details.
+
+##     You should have received a copy of the GNU Affero General Public License
+##     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
 @author: assem
 @contact: assem.ch [at] gmail.com
-@license: GPL
-@organization: Waiting support 
+@license: AGPL
+
 
 
 @todo: use optparse to organise this script as a console command
