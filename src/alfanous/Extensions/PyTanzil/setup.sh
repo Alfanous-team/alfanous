df43578363e1d23a15ff55b577f6025e42c732a0
Assem Chelli <assem.ch@gmail.com>
2011-12-03 16:36:07 +0000

    Remove src.alfanous.Extensions
    Former-commit-id: 9a61194f088e73821ed08c0157519ffb1dceff2c

--- src/alfanous/Extensions/PyTanzil/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-(python setup.py sdist --format=zip
-python setup.py bdist_egg
-python setup.py bdist_rpm 
-python setup.py bdist_wininst
-python setup.py register
-python setup.py  upload)>>test.text
-
