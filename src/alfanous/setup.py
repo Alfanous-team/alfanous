c7bf2935623c5d3344972cf18d653ca0d5e87795
Abdellah Chelli (Sneetsher) <abdellahchelli@gmail.com>
2011-12-05 20:14:26 +0000

    Clean up & update setup of alfanous module

--- src/alfanous/setup.py
+++ src/alfanous/setup.py
@@ -1,47 +1,45 @@
-#!python
-#coding=utf-8
+#!/usr/bin/env python
+# coding = utf-8
 
 import glob
 
 
-
 try:
-    from setuptools import setup, find_packages
+	from setuptools import setup, find_packages
 except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    
-    
-    
-
-
+	from ez_setup import use_setuptools
+	use_setuptools()
 
 
 setup(
 	name="alfanous",
+	description="Quranic search engine API",
 	version=0.4,
-	package_dir={'alfanous':'.'},
-	packages=find_packages(where='..', exclude=()),
+	platforms="ALL",
+	license="GPL",
+	#packages=find_packages(where='..', exclude=()),
+	packages=['alfanous'],
+	install_requires=['pyparsing','harvestman'],# 'whoosh == 0.3.18',
+
 	author="Assem Chelli",
 	author_email="assem.ch@gmail.com",
-	description="Quranic search engine API",
-    long_description="""Alfanous is a search engine provide the simple and advanced search in the Holy Qur'an and more features...""",
-	license="GPL",
+	maintainer="Assem Chelli",
+	maintainer_email="assem.ch@gmail.com",
+
+	package_dir={'alfanous':'.'},
+	long_description="""Alfanous is a search engine provide the simple and advanced search in the Holy Qur'an and more features...""",
 	keywords="quran search indexing engine alfanous",
-	url="http://www.alfanous.org",
-	download_url="https://sourceforge.net/projects/alfanous/files",
-	install_requires=['pyparsing','harvestman'],# 'whoosh == 0.3.18',
+	url="http://www.alfanous.org/",
+	download_url="https://sourceforge.net/projects/alfanous/files/",
 
 	include_package_data=True,
 
-    #data_files=[ ('indexes/main',glob.glob('indexes/main/*')),('indexes/extend',glob.glob('indexes/extend/*'))],#,('indexes/extend',glob.glob('indexes/extend/*'))
-   
-    
+	#data_files=[ ('indexes/main',glob.glob('indexes/main/*')),('indexes/extend',glob.glob('indexes/extend/*'))],#,('indexes/extend',glob.glob('indexes/extend/*'))
+
 	zip_safe=True,
 
-	
 	classifiers=[
-	"Development Status :: 3 - Alpha",
+	"Development Status :: 4 - Beta",
 	"Intended Audience :: Developers",
 	"License :: OSI Approved :: GNU General Public License (GPL)",
 	"Natural Language :: Arabic",
@@ -49,5 +47,4 @@ setup(
 	"Programming Language :: Python :: 2.6",
 	"Topic :: Software Development :: Libraries :: Python Modules",
 	],
-	
 )
