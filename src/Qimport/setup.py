6cf97855b3f182a89df0774622bf43caff5db7dd
Abdellah Chelli (Sneetsher) <abdellahchelli@gmail.com>
2011-12-05 19:48:24 +0000

    Clean up & update setup of Qimport module
    Former-commit-id: 7769c228b191ef3bd8ac8df7c01c244b8097be57

--- src/Qimport/setup.py
+++ src/Qimport/setup.py
@@ -1,4 +1,4 @@
-#!python
+#!/usr/bin/env python
 #coding=utf-8
 
 
@@ -7,25 +7,38 @@ from setuptools import setup
 
 setup(
 	name="Qimport",
+	description="Importing system for quranic indexes",
 	version=0.1,
-	package_dir={'Qimport':'.'},
+	platforms="ALL",
+	license="GPL",
 	packages=["Qimport"],
+	install_requires=["whoosh","PyCorpus"],
+
 	author="Assem Chelli",
 	author_email="assem.ch@gmail.com",
-	description="importing system for quranic indexes",
-    long_description=""" Importing System for alfanous Search engine""",
-	license="GPL",
+	maintainer="Assem Chelli",
+	maintainer_email="assem.ch@gmail.com",
+
+	package_dir={'Qimport':'.'},
+	long_description=""" Importing system for Alfanous search engine""",
 	keywords="search quran islam alfanous arabic",
-	url="alfanous.sf.net/cms",
-	download_url="",
-	install_requires=["whoosh","PyCorpus"],
+	url="http://www.alfanous.org/",
+	download_url="https://sourceforge.net/projects/alfanous/files/",
+
 	include_package_data=True,
-	
+
 	data_files=[ ('.','./urls.txt'),('.','./GPL.txt'),('.','./config.xml')],
-    
+
 	zip_safe=True,
 
-	
-
-	
+	classifiers=[
+	"Development Status :: 4 - Beta",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: GNU General Public License (GPL)",
+	"Natural Language :: Arabic",
+	"Natural Language :: English",
+	"Operating System :: OS Independent",
+	"Programming Language :: Python :: 2.6",
+	"Topic :: Software Development :: Libraries :: Python Modules",
+	]
 )
