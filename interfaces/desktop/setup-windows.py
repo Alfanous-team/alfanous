ea2b066644e88f0bc2e23852ba14da267ecce7c7
Abdellah Chelli (Sneetsher) <abdellahchelli@gmail.com>
2011-12-08 23:37:20 +0000

    Clean alfanousDesktop setup & Add new python launcher for it
    Former-commit-id: 11ce621ae2c1cb7703df8c3ea503ea3cfd3cd874

--- interfaces/desktop/setup-windows.py
+++ interfaces/desktop/setup-windows.py
@@ -2,62 +2,62 @@
 #coding=utf-8
 
 
-        
-        
 from setuptools import setup, find_packages
+
 try:
 	import py2exe
 except:
 	pass
-#import py2app
+
 import glob
+
 
 setup(
 	name="alfanousDesktop",
-	version=0.1,
-	package_dir={'alfanousDesktop':'.'},
+	description="Desktop interface for alfanous Quranic search engine API",
+	version=0.4,
+	platforms="ALL",
+	license="GPL",
 	packages=["alfanousDesktop"],
-	#data_files=[("install","files")],
+	install_requires=['alfanous','configobj','pyqt'],#,''
+
 	author="Assem Chelli",
 	author_email="assem.ch@gmail.com",
-	description="a desktop interface for alfanous Quranic search engine API",
-    long_description="""""",
-	license="GPL",
-	keywords="quran search indexing engine alfanous",
-	url="http://alfanous.sourceforge.net/cms",
-	download_url="https://sourceforge.net/projects/alfanous/files",
-	install_requires=['alfanous','configobj','pyqt'],#,''
-	entry_points={
-        'gui_scripts': [
-            'alfanousQT = alfanousDesktop.Gui:main',
-        ]
-       },
+	maintainer="Assem Chelli",
+	maintainer_email="assem.ch@gmail.com",
 
-    
+	package_dir={'alfanousDesktop':'.'},
+	long_description="""A PyQt GUI interface for alfanous Quranic search engine API""",
+	keywords="quran search indexing engine alfanous",
+	url="http://www.alfanous.org/",
+	download_url="https://sourceforge.net/projects/alfanous/files/",
+
+	#data_files=[("install","files")],
+	#data_files=[('.','config.ini'),('index',glob.glob('resources/*.*'))],
+
 	include_package_data=True,
-  #data_files=[('.','config.ini'),('index',glob.glob('resources/*.*'))    ],
-   
-   windows = [
-        {
-            "script": "Gui.py",
-            "icon_resources": [(1, "resources/Alfanous.ico")]
-			
-        }
-    ],
-     options={"py2exe" : {"includes" : ["sip"]}},
-    
-    app=["Gui.py"],
+
+	entry_points={
+		'gui_scripts': [
+			'alfanousQT = alfanousDesktop.Gui:main',
+		]
+		},
+	windows = [{
+			"script": "Gui.py",
+			"icon_resources": [(1, "resources/Alfanous.ico")]
+		}],
+	options={"py2exe" : {"includes" : ["sip"]}},
+	app=["Gui.py"],
 
 	zip_safe=True,
 
-	
 	classifiers=[
 	"Development Status :: 4 - Beta",
 	"Intended Audience :: Users",
 	"License :: OSI Approved :: GNU General Public License (GPL)",
 	"Natural Language :: Arabic",
+	"Natural Language :: English",
 	"Operating System :: OS Independent",
 	"Programming Language :: Python :: 2.6",
-	],
-	
+	]
 )
