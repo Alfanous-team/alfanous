18e482148b3be2032b704a90503541f841672c9b
Assem Chelli <assem.ch@gmail.com>
2011-12-11 16:34:47 +0000

    upgrade DEB generating  scripts
    Former-commit-id: 8315f22ab8945166fcb6edc8fda4426112af57d3

--- dist/DEB/remove-svn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "recursively removing .svn folders from"
-pwd
-rm -rf `find . -type d -name .svn`
-
--- dist/DEB/upload-changes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dput ppa:sabily.team/ppa alfanous_$1_source.changes
-dput ppa:team4alfanous/alfanous alfanous_$1_source.changes
-
