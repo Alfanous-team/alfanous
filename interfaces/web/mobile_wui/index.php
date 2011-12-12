cc8778bce46bfaf00a3840f3e1ef78f7db9f682a
Abdellah Chelli (Sneetsher) <abdellahchelli@gmail.com>
2011-12-12 00:02:56 +0000

    Fix empty results bug #902815,Mobile WUI
    Former-commit-id: bd6ea6b286334816659c8d4f3b47d295d736a6df

--- interfaces/web/mobile_wui/index.php
+++ interfaces/web/mobile_wui/index.php
@@ -78,8 +78,6 @@ if ($show_result) {
 	fclose($handle);
 
 	$json = json_decode($contents);
-	# debug:
-	#var_dump($json);
 };
 
 # XHTML header
@@ -178,6 +176,18 @@ print('
 <body dir="rtl">
 ');
 
+/*
+if ($show_result) {
+	# debug:
+	# print raw json
+	printf('<!-- DEBUG --><pre>%s</pre>', htmlspecialchars($contents));
+	# dump json decoded object
+	print('<pre>');
+	var_dump($json);
+	print('</pre>');
+};
+*/
+
 # Search form template
 $search_form='
 	<form id="form_%1$s" class="form" method="get" action="index.php">
@@ -194,75 +204,77 @@ name="search" title="search" inputmode="arabic" />
 # Top search form
 printf($search_form,1);
 
-if ($show_result and $json) {
+if ($show_result and $json and $json->interval->total) {
 	# case: some results
 
 	# Pages control
 	$nb_pages = floor(($json->interval->total- 1) / 10)+ 1;
-	$page_nb = floor(($json->interval->start- 1) / 10)+ 1;
-	# Alfanous JSON service doesn't serve more than 100 pages.
-	$hit_page_limit = False;
-	if ($nb_pages > 100) {
-		$hit_page_limit = True;
-		$nb_pages = 100;
-	};
-
-	$results_pages = sprintf("<div class='pages'>\nالنتائج: %s-%s\\%s الصفحات:"
-		,$json->interval->start
-		,$json->interval->end
-		,$json->interval->total);
-	for ($i = 1; $i <= $nb_pages; $i++) {
-		if ($i == $page_nb) {
-			$results_pages .= " ". $i;
-		}
-		else {
-			$results_pages .= sprintf(" <a href='%s'>%s</a>",
-				htmlspecialchars("index.php?" . $query_search . "&page=" . $i),
-				$i);
+	if ($nb_pages) {
+		$page_nb = floor(($json->interval->start- 1) / 10)+ 1;
+		# Alfanous JSON service doesn't serve more than 100 pages.
+		$hit_page_limit = False;
+		if ($nb_pages > 100) {
+			$hit_page_limit = True;
+			$nb_pages = 100;
 		};
+
+		$results_pages = sprintf("<div class='pages'>\nالنتائج: %s-%s\\%s الصفحات:"
+			,$json->interval->start
+			,$json->interval->end
+			,$json->interval->total);
+		for ($i = 1; $i <= $nb_pages; $i++) {
+			if ($i == $page_nb) {
+				$results_pages .= " ". $i;
+			}
+			else {
+				$results_pages .= sprintf(" <a href='%s'>%s</a>",
+					htmlspecialchars("index.php?" . $query_search . "&page=" . $i),
+					$i);
+			};
+		};
+		# show a sign for pages limit '...', a part of pages are not listed
+		if ($hit_page_limit) $results_pages .= " ...";
+		$results_pages .= "</div>\n";
+		print($results_pages);
+
+		# Results listing
+		$results = "<div id='search_result'>";
+		for( $i = $json->interval->start; $i <= $json->interval->end; $i++) {
+			$results .= sprintf ("
+			<div class='result_item'>
+				<div><span class='item_number'>%s </span>
+					<span class='aya_info'> (%s %s) </span></div>
+				<div class='quran'> [ %s ] </div>
+				<div class='aya_details'>
+		‎			الكلمات %s\\%s - الأحرف %s\\%s - ألفاظ الجلالة %s\\%s
+					<br /> الحزب %s - الصفحة %s</div>
+			</div>"
+			,$i
+			,$json->ayas->$i->sura->name,$json->ayas->$i->aya->id
+			,$json->ayas->$i->aya->text
+			,$json->ayas->$i->stat->words,$json->ayas->$i->sura->stat->words
+			,$json->ayas->$i->stat->letters,$json->ayas->$i->sura->stat->letters
+			,$json->ayas->$i->stat->godnames,$json->ayas->$i->sura->stat->godnames
+			,$json->ayas->$i->position->hizb,$json->ayas->$i->position->page
+			);
+		};
+		$results .= "</div>";
+		print($results);
+
+		# Pages control again
+		print($results_pages);
+
+		printf($search_form,2);
 	};
-	# show a sign for pages limit '...', a part of pages are not listed
-	if ($hit_page_limit) $results_pages .= " ...";
-	$results_pages .= "</div>\n";
-	print($results_pages);
-
-	# Results listing
-	$results = "<div id='search_result'>";
-	for( $i = $json->interval->start; $i <= $json->interval->end; $i++) {
-		$results .= sprintf ("
-		<div class='result_item'>
-			<div><span class='item_number'>%s </span>
-				<span class='aya_info'> (%s %s) </span></div>
-			<div class='quran'> [ %s ] </div>
-			<div class='aya_details'>
-	‎			الكلمات %s\\%s - الأحرف %s\\%s - ألفاظ الجلالة %s\\%s
-				<br /> الحزب %s - الصفحة %s</div>
-		</div>"
-		,$i
-		,$json->ayas->$i->sura->name,$json->ayas->$i->aya->id
-		,$json->ayas->$i->aya->text
-		,$json->ayas->$i->stat->words,$json->ayas->$i->sura->stat->words
-		,$json->ayas->$i->stat->letters,$json->ayas->$i->sura->stat->letters
-		,$json->ayas->$i->stat->godnames,$json->ayas->$i->sura->stat->godnames
-		,$json->ayas->$i->position->hizb,$json->ayas->$i->position->page
-		);
-	};
-	$results .= "</div>";
-	print($results);
-
-	# Pages control again
-	print($results_pages);
-
-	printf($search_form,2);
 }
-elseif ($show_result and !$json and $search) {
+elseif ($show_result and $search) {
 	# case: no results
 	print('
 	<hr />
 	<p>:/ لم يعثر الفانوس على أي أيات تنطبق عليها معطيات البحث.</p>
 	');
 }
-else {
+else{
 	# case: no query
 	# tobe replaced with Arabic paragraph
 	print('
