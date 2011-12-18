1cc03248a5f200b95fbc6c80f9e7b7e16e6a796c
Abdellah Chelli (Sneetsher) <abdellahchelli@gmail.com>
2011-12-18 21:31:20 +0000

    Add L10n/I18n support Add English as default & Arabic Add Tashkil option Add Fazzy search option Add Theme option (Not yet ready)
    Former-commit-id: 473a4007fec6404a122c3c8cda0ed4a8b4719cbc

--- interfaces/web/mobile_wui/index.php
+++ interfaces/web/mobile_wui/index.php
@@ -21,30 +21,71 @@
 *******************************************************************************/
 
 /*
-    Output should be valid for Mobile Web Standards (ie: XHTML1.1 Basic + CSS1)
-    There is only one exception concerning RTL support
-    <http://validator.w3.org/mobile/> helps much to check standards
+  Highlights:
+   - Output should be valid for Mobile Web Standards (ie: XHTML1.1 Basic + CSS1)
+   - There is only one exception concerning RTL support
+   - <http://validator.w3.org/mobile/> helps much to check standards
+   - CSS is embedded within XHTML output to avoid many requests
+   - For I18n/L10n, it uses php-gettext (As fallback)
 */
+
+# Constants
+define("DEBUG", 0);
+define("M_EXEC", 1);
+define("ROOT_DIR", realpath("./")); # or dirname(__FILE__);
+define("LOCALE_DIR", ROOT_DIR ."/locale");
+define("DEFAULT_LOCALE", "en");
+define("THEME_DIR", ROOT_DIR ."/themes/");
+define("DEFAULT_THEME", "std");
+
+# Gettext
+require_once("./php-gettext/gettext.inc");
+$domain = "AlfanousMobile";
+$locales_list = array("en", "ar");
+$encoding = "UTF-8";
+
+# Theme
+$themes_list = array("std");
+
+# Check GET parameters
+$search = isset($_GET["search"]) ? $_GET["search"] : "";
+$page = isset($_GET["page"]) ? $_GET["page"] : 1;
+$language = isset($_GET["language"]) ? $_GET["language"] : DEFAULT_LOCALE;
+$tashkil = isset($_GET["tashkil"]) ? True : False;
+$fazzy = isset($_GET["fazzy"]) ? True : False;
+
+
+# Gettext
+# set language
+putenv("LC_ALL=$language");
+T_setlocale(LC_ALL, $language);
+# set local
+T_bindtextdomain($domain, LOCALE_DIR);
+T_bind_textdomain_codeset($domain, $encoding);
+T_textdomain($domain);
+# import localized texts
+require_once "./mobiletext.php";
+$mt = new MT();
+
 
 # HTTP header
 header("Content-Type: application/xhtml+xml; charset=UTF-8");
 header("Content-Language: ar");
 
 header("Cache-Control: max-age=3600, must-revalidate");
-header(sprintf("Last-Modified: %s", date("r", filemtime($_SERVER['SCRIPT_FILENAME']))));
+header(sprintf(
+	"Last-Modified: %s",
+	date("r", filemtime($_SERVER['SCRIPT_FILENAME']))
+	));
 
-# Check GET parameters
-	$search = isset($_GET["search"]) ? $_GET["search"] : "";
-	$page = isset($_GET["page"]) ? $_GET["page"] : 1;
-
+# Query JSON service
 if ($search) {
 
-	# Hidden parameters
+	# Hidden JSON query parameters
 	$sortedby="mushaf";
 	$recitation="Mishary Rashid Alafasy";
 	$translation="None";
 	$highlight="css";
-	#$fuzzy="yes"; not stable yet
 
 	# Encode JSON query URL
 	$query_site = "http://www.alfanous.org/json?";
@@ -53,8 +94,8 @@ if ($search) {
 	$query_string =  "&sortedby=" . urlencode($sortedby)
 		. "&recitation=" . urlencode($recitation)
 		. "&translation=" . urlencode($translation)
-		. "&highlight=" . urlencode($highlight);
-		#. "&fuzzy=" . urlencode($fuzzy);
+		. "&highlight=" . urlencode($highlight)
+		. (($fazzy)?"&fuzzy=yes":"");
 
 	# JSON query
 	$handle = fopen($query_site . $query_search . $query_page . $query_string,
@@ -64,151 +105,169 @@ if ($search) {
 
 	$json = json_decode($contents);
 };
-
-# XHTML header
-print('
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN"
     "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 
-	<title>Alfanous</title>
+	<title><? echo $mt->ALFANOUS; ?></title>
 
 	<meta http-equiv="Content-Type" content="application/xhtml+xml; 
 charset=UTF-8" />
-	<link rel="shortcut icon" type="image/gif" href="icon.gif" />
-');
+	<link rel="shortcut icon" type="image/gif" href="./icon.gif" />
 
+<?php
 # CSS1 style
-$css = file_get_contents('basic.css');
-printf ('
-<style type="text/css">
-%s
-</style>', $css);
+$css = file_get_contents('./basic.css');
+echo "<style type=\"text/css\">\n$css\n</style>";
+?>
 
-# Body + Search form
-print('
 </head>
-<body dir="rtl">
-');
+<body<? echo ($mt->RTL)?" dir=\"rtl\"":""; ?>>
 
-/*
-if ($search) {
-	# debug:
-	# print raw json
-	printf('<!-- DEBUG --><pre>%s</pre>', htmlspecialchars($contents));
-	# dump json decoded object
-	print('<pre>');
-	var_dump($json);
-	print('</pre>');
-};
-*/
+<? if (DEBUG and $search) var_dump($json); ?>
 
-# Search form template
-$search_form='
-	<form id="form_%1$s" class="form" method="get" action="index.php">
-		<div id="search_form_%1$s" class="search_form">
-			<img src="icon.gif" width="32" height="32" alt="logo" />
-			<span>الفانوس</span>
-			<input id="search_box_%1$s" class="search_box" type="text" 
+	<form id="form_top" class="form" method="get" action="index.php">
+		<div class="search_form">
+			<img src="./icon.gif" width="32" height="32" alt="logo" />
+			<span><? echo $mt->ALFANOUS; ?></span>
+			<input class="search_box" type="text" 
 name="search" title="search" inputmode="arabic" />
-			<input id="submit_%1$s" class="submit" type="submit" value="بحث" />
+			<input class="submit" type="submit" value="<? echo $mt->SEARCH; ?>" />
+			<br />
+			<input name="tashkil" type="checkbox"
+<? echo ($tashkil)?" checked=\"checked\"":""; ?> value="yes" />
+<? echo $mt->TASHKIL; ?> | 
+			<input name="fazzy" type="checkbox"
+<? echo ($fazzy)?" checked=\"checked\"":""; ?> value="yes" />
+<? echo $mt->FAZZY; ?>
+			<br />
+			<? echo $mt->LANGUAGE; ?>:
+			<select name="language">
+				<? foreach ($locales_list as $l ) {
+					echo "<option ".(($l == $language)?"selected=\"selected\" ":"")."value=\"$l\">$l</option>";
+				}; ?>
+			</select> | 
+			<? echo $mt->THEME; ?>:
+			<select name="theme">
+				<? foreach ($themes_list as $t ) {
+					echo "<option ".(($t == $theme)?"selected=\"selected\" ":"")."value=\"$t\">$t</option>";
+				}; ?>
+			</select>
 		</div>
 	</form>
-';
 
-# Top search form
-printf($search_form,1);
-
-if ($search and $json and $json->interval->total) {
-	# case: some results
+<?php
+if ($search and $json and $json->interval->total): # case: some results
 
 	# Pages control
 	$nb_pages = floor(($json->interval->total- 1) / 10)+ 1;
-	if ($nb_pages) {
-		$page_nb = floor(($json->interval->start- 1) / 10)+ 1;
-		# Alfanous JSON service doesn't serve more than 100 pages.
-		$hit_page_limit = False;
-		if ($nb_pages > 100) {
-			$hit_page_limit = True;
-			$nb_pages = 100;
-		};
-
-		$results_pages = sprintf("<div class='pages'>\nالنتائج: %s-%s\\%s الصفحات:"
-			,$json->interval->start
-			,$json->interval->end
-			,$json->interval->total);
-		for ($i = 1; $i <= $nb_pages; $i++) {
-			if ($i == $page_nb) {
-				$results_pages .= " ". $i;
-			}
-			else {
-				$results_pages .= sprintf(" <a href='%s'>%s</a>",
-					htmlspecialchars("index.php?" . $query_search . "&page=" . $i),
-					$i);
-			};
-		};
-		# show a sign for pages limit '...', a part of pages are not listed
-		if ($hit_page_limit) $results_pages .= " ...";
-		$results_pages .= "</div>\n";
-		print($results_pages);
-
-		# Results listing
-		$results = "<div id='search_result'>";
-		for( $i = $json->interval->start; $i <= $json->interval->end; $i++) {
-			$results .= sprintf ("
-			<div class='result_item'>
-				<div><span class='item_number'>%s </span>
-					<span class='aya_info'> (%s %s) </span></div>
-				<div class='quran'> [ %s ] </div>
-				<div class='aya_details'>
-		‎			الكلمات %s\\%s - الأحرف %s\\%s - ألفاظ الجلالة %s\\%s
-					<br /> الحزب %s - الصفحة %s</div>
-			</div>"
-			,$i
-			,$json->ayas->$i->sura->name,$json->ayas->$i->aya->id
-			,$json->ayas->$i->aya->text
-			,$json->ayas->$i->stat->words,$json->ayas->$i->sura->stat->words
-			,$json->ayas->$i->stat->letters,$json->ayas->$i->sura->stat->letters
-			,$json->ayas->$i->stat->godnames,$json->ayas->$i->sura->stat->godnames
-			,$json->ayas->$i->position->hizb,$json->ayas->$i->position->page
-			);
-		};
-		$results .= "</div>";
-		print($results);
-
-		# Pages control again
-		print($results_pages);
-
-		printf($search_form,2);
+	$page_nb = floor(($json->interval->start- 1) / 10)+ 1;
+	# Alfanous JSON service doesn't serve more than 100 pages.
+	$hit_page_limit = False;
+	if ($nb_pages > 100) {
+		$hit_page_limit = True;
+		$nb_pages = 100;
 	};
-}
-elseif ($search) {
-	# case: no results
-	print('
-	<hr />
-	<p>:/ لم يعثر الفانوس على أي أيات تنطبق عليها معطيات البحث.</p>
-	');
-}
-else{
-	# case: no query
-	# tobe replaced with Arabic paragraph
-	print('
-	<hr />
-	<p class="align-left" dir="ltr">Alfanous is a Quranic search engine offers 
-	simple and advanced search services in the whole information that Holy 
-	Qur’an contains. it is based on the modern approach of information 
-	retrieval to get a good-stability and a high-speed search. We want 
-	implement some additional features like Highlight, Suggestions, Scoring 
-	…etc.</p>
-	');
-};
-
-# Close body
-print('
-</body>
-</html>
-');
-
 ?>
 
+	<div class='pages'><? echo $mt->RESULTS; ?>: 
+		<? echo $json->interval->start; ?>-<? echo $json->interval->end; ?><? echo ($mt->RTL)?"\\":"/"; ?><? echo $json->interval->total; ?> 
+		<? echo $mt->PAGES; ?>:
+
+<?php
+	$results_pages = "";
+	for ($i = 1; $i <= $nb_pages; $i++) {
+		if ($i == $page_nb) {
+			$results_pages .= " ". $i;
+		}
+		else {
+			$results_pages .= sprintf(" <a href='%s'>%s</a>",
+				htmlspecialchars("index.php?" . $query_search . "&page=" . $i),
+				$i);
+		};
+	};
+	# show a sign for pages limit '...', a part of pages are not listed
+	if ($hit_page_limit) $results_pages .= " ...";
+	print($results_pages);
+?>
+	</div>
+
+	<div id='search_result'>
+<?php
+	# Results listing
+	for( $i = $json->interval->start; $i <= $json->interval->end; $i++): ?>
+
+		<div class='result_item'>
+			<div dir='rtl' class='align-right'><span class='item_number'><? echo $i; ?> </span>
+				<span class='aya_info'> (<? echo $json->ayas->$i->sura->name; ?> <? echo $json->ayas->$i->aya->id; ?>) </span></div>
+			<div class='quran align-right' dir='rtl'> [ <? echo ($tashkil)?$json->ayas->$i->aya->text:preg_replace("/[\x{064B}-\x{065F}]/u", "", $json->ayas->$i->aya->text); ?> ] </div>
+			<div class='aya_details <? echo ($mt->RTL)?"align-right":"align-left"; ?>'>
+	‎			<? echo $mt->WORDS; ?> 
+	<? echo $json->ayas->$i->stat->words; ?><? echo ($mt->RTL)?"\\":"/"; ?><? echo $json->ayas->$i->sura->stat->words; ?> 
+				- <? echo $mt->LETTERS; ?> 
+	<? echo $json->ayas->$i->stat->letters; ?><? echo ($mt->RTL)?"\\":"/"; ?><? echo $json->ayas->$i->sura->stat->letters; ?> 
+				- <? echo $mt->GODNAMES; ?> 
+	<? echo $json->ayas->$i->stat->godnames; ?><? echo ($mt->RTL)?"\\":"/"; ?><? echo $json->ayas->$i->sura->stat->godnames; ?>
+				<br />
+				<? echo $mt->HIZB; ?> 
+	<? echo $json->ayas->$i->position->hizb; ?> 
+				- <? echo $mt->PAGE; ?> 
+	<? echo $json->ayas->$i->position->page; ?></div>
+		</div>
+
+<? endfor; ?>
+	</div>
+
+	<div class='pages'>
+		<? echo $mt->RESULTS; ?>: 
+		<? echo $json->interval->start; ?>-<? echo $json->interval->end; ?><? echo ($mt->RTL)?"\\":"/"; ?><? echo $json->interval->total; ?> 
+		<? echo $mt->PAGES; ?>:
+		<? print($results_pages); ?>
+	</div>
+
+	<form id="form_top" class="form" method="get" action="index.php">
+		<div class="search_form">
+			<img src="./icon.gif" width="32" height="32" alt="logo" />
+			<span><? echo $mt->ALFANOUS; ?></span>
+			<input class="search_box" type="text" 
+name="search" title="search" inputmode="arabic" />
+			<input class="submit" type="submit" value="<? echo $mt->SEARCH; ?>" />
+			<br />
+			<input name="tashkil" type="checkbox"
+<? echo ($tashkil)?" checked=\"checked\"":""; ?> value="yes" />
+<? echo $mt->TASHKIL; ?> | 
+			<input name="fazzy" type="checkbox"
+<? echo ($fazzy)?" checked=\"checked\"":""; ?> value="yes" />
+<? echo $mt->FAZZY; ?>
+			<br />
+			<? echo $mt->LANGUAGE; ?>:
+			<select name="language">
+				<? foreach ($locales_list as $l ) {
+					echo "<option ".(($l == $language)?"selected=\"selected\" ":"")."value=\"$l\">$l</option>";
+				}; ?>
+			</select> | 
+			<? echo $mt->THEME; ?>:
+			<select name="theme">
+				<? foreach ($themes_list as $t ) {
+					echo "<option ".(($t == $theme)?"selected=\"selected\" ":"")."value=\"$t\">$t</option>";
+				}; ?>
+			</select>
+		</div>
+	</form>
+
+<? elseif ($search): # case: no results ?>
+
+	<hr />
+	<p><? echo $mt->NORESULTS; ?></p>
+
+<? else: # case: no query ?>
+
+	<hr />
+	<p><? echo $mt->ALFANOUS_INTRO; ?></p>
+
+<? endif; ?>
+
+</body>
+</html>
