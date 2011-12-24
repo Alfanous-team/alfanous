001a5c1e3d66eac3426dd51c3b308ad3209e339f
Assem Chelli <assem.ch@gmail.com>
2011-12-24 10:32:26 +0000

    a bat script to automate the generating of windows installer
    Former-commit-id: 48c0056b66481b49156eefa93b6b257407040f92

--- dist/nsis/make_nsis_installer.win.bat
+++ dist/nsis/make_nsis_installer.win.bat
@@ -1,10 +1,20 @@
 @echo off
 
-cd ..\..\interfaces\desktop
-python setup.py py2exe
+
+set VERSION="0.4.3"
+set PYTHON="C:\Python26\python.exe"
+set MAKENSIS="H:\developement\nsis\makensis.exe"
+
+cd ..\..\interfaces\desktop 
+rem %PYTHON% setup.py py2exe
 
 cd ..\..\
-rem makensis nsis_installer...
+%MAKENSIS% make_installer.nsi
 
-move alfanousInstaller.exe output\
+mkdir output
+mkdir output\%VERSION%
+move alfanousInstaller.exe output\%VERSION%\alfanousInstallerV%VERSION%.exe 
 rmdir /s /q interfaces\desktop\dist 
+
+echo "end."
+pause
