54d3d65290a5ede83b8a51794f8e6a31217ab002
Assem Chelli <assem.ch@gmail.com>
2012-08-02 03:20:25 +0100

    import Qpaginate in the main module to be used by alfanous-test.main

--- src/alfanous/main.py
+++ src/alfanous/main.py
@@ -36,7 +36,7 @@ TODO add qurany project for Subjects in english
 #from alfanous.dynamic_resources.arabicnames_dyn import ara2eng_names
 from alfanous.Searching import QSearcher, QReader
 from alfanous.Indexing import QseDocIndex, ExtDocIndex, BasicDocIndex
-from alfanous.ResultsProcessing import Qhighlight #, QPaginate , QFilter
+from alfanous.ResultsProcessing import Qhighlight , QPaginate# , QFilter
 from alfanous.QueryProcessing import QuranicParser, StandardParser, FuzzyQuranicParser #,  ArabicParser
 from alfanous.Suggestions import  QAyaSpellChecker, QSubjectSpellChecker, concat_suggestions, QWordChecker #, QSuggester
 
