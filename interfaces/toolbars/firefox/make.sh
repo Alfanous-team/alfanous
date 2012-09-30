5ff5e6d6de86de85cd8044ab3231d5c8c862d880
zakaria <zakaria@PC-zakaria.(none)>
2012-09-30 19:15:36 +0100

    Enabling history search , opening URLs in new tab instead of same one , using new icons added by Muslih AlAqqad, Adding Assem Chelli and Muslih as contributors in Alfanous Toolbar
    Former-commit-id: 5f6b411349915ecd67f0fd7635a860f1002b7495

--- interfaces/toolbars/firefox/make.sh
+++ interfaces/toolbars/firefox/make.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-
 # 	make file 
 #	created on Wednsday, August 15th, 2012 at 13:00
+#	updated on 
 #	by SMAHI Zakaria
 #	zakaria08esi@gmail.com
 
 cd chrome
-zip -r alfanoustoolbar.jar content/* skin/*
+zip alfanoustoolbar.jar -r content/* skin/*
 cd ..
 zip alfanoustoolbar.xpi install.rdf chrome.manifest chrome/alfanoustoolbar.jar
 
