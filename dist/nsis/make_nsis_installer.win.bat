3420156c70f65247767a0fb2f95f4be4ed6bc8bd
Assem Chelli <assem.ch@gmail.com>
2012-11-20 22:37:47 +0100

    move make_installer.nsi script inside dist\nsis folder

--- dist/nsis/make_nsis_installer.win.bat
+++ dist/nsis/make_nsis_installer.win.bat
@@ -8,9 +8,10 @@ set MAKENSIS="H:\developement\nsis\makensis.exe"
 cd ..\..\interfaces\desktop 
 rem %PYTHON% setup.py py2exe
 
-cd ..\..\
+cd ..\..\dist\nsis
 %MAKENSIS% make_installer.nsi
 
+cd ..\..\
 mkdir output
 mkdir output\%VERSION%
 move alfanousInstaller.exe output\%VERSION%\alfanousInstallerV%VERSION%.exe 
