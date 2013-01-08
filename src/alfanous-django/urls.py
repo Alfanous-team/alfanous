329f4977c775603f90ab66131dfde2b6343f6f4c
Assem Chelli <assem.ch@gmail.com>
2013-01-08 12:34:10 +0100

    Create a block in the wui django template shown only if no request

--- src/alfanous-django/urls.py
+++ src/alfanous-django/urls.py
@@ -7,12 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns( '',
     # Examples:
     # url(r'^$', 'alfanousDjango.views.home', name='home'),
-    url( r'^jos2/', 'alfanous-django.wui.views.jos2' ),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url( r'^jos2', 'alfanous-django.wui.views.jos2' ),
     url( r'^admin/', include( admin.site.urls ) ),
     url( r'^(?P<language>\w{2})/(?P<unit>\w{3,15})/', 'alfanous-django.wui.views.results' ),
     url( r'^(?P<language>\w{2})/', 'alfanous-django.wui.views.results' ),
