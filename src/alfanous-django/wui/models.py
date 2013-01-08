93e804f03a0d21061ae13d6d686371542647ed3c
Assem Chelli <assem.ch@gmail.com>
2013-01-08 15:45:07 +0100

    preparing the access control to the WUI and the JOS2

--- src/alfanous-django/wui/models.py
+++ src/alfanous-django/wui/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 class Visit (models.Model):
     id = models.AutoField(primary_key=True)
+    ip = models.CharField( max_length = 15 ) #may be this should be the key
+    host = models.CharField( max_length = 255 )
+    referer = models.CharField( max_length = 255 )
+    request = models.CharField( max_length = 255 )
     query = models.CharField( max_length=255)
     sorted_by = models.CharField( max_length=10)
     recitation = models.CharField( max_length=30)
