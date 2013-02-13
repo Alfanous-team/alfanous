4304e10f4dc9d16c6f3b6f8ba1702071980588cb
Assem Chelli <assem.ch@gmail.com>
2013-02-13 13:20:39 +0100

    Fix a bug in romanization by converting all strings to unicode,fix #182
    (Romanization of some characters of ISO233 raises an exception )
    
    Former-commit-id: 9df4813b4ac94800abda669627615bd799be9672

--- src/alfanous/Romanization.py
+++ src/alfanous/Romanization.py
@@ -18,136 +18,136 @@
 
 
 # Buckwalter Romanization  letters mapping
-BUCKWALTER2UNICODE = {"'": u"\u0621", # hamza-on-the-line
-                "|": u"\u0622", # madda
-                ">": u"\u0623", # hamza-on-'alif
-                "&": u"\u0624", # hamza-on-waaw
-                "<": u"\u0625", # hamza-under-'alif
-                "}": u"\u0626", # hamza-on-yaa'
-                "A": u"\u0627", # bare 'alif
-                "b": u"\u0628", # baa'
-                "p": u"\u0629", # taa' marbuuTa
-                "t": u"\u062A", # taa'
-                "v": u"\u062B", # thaa'
-                "j": u"\u062C", # jiim
-                "H": u"\u062D", # Haa'
-                "x": u"\u062E", # khaa'
-                "d": u"\u062F", # daal
-                "*": u"\u0630", # dhaal
-                "r": u"\u0631", # raa'
-                "z": u"\u0632", # zaay
-                "s": u"\u0633", # siin
-                "$": u"\u0634", # shiin
-                "S": u"\u0635", # Saad
-                "D": u"\u0636", # Daad
-                "T": u"\u0637", # Taa'
-                "Z": u"\u0638", # Zaa' (DHaa')
-                "E": u"\u0639", # cayn
-                "g": u"\u063A", # ghayn
-                "_": u"\u0640", # taTwiil
-                "f": u"\u0641", # faa'
-                "q": u"\u0642", # qaaf
-                "k": u"\u0643", # kaaf
-                "l": u"\u0644", # laam
-                "m": u"\u0645", # miim
-                "n": u"\u0646", # nuun
-                "h": u"\u0647", # haa'
-                "w": u"\u0648", # waaw
-                "Y": u"\u0649", # 'alif maqSuura
-                "y": u"\u064A", # yaa'
-                "F": u"\u064B", # fatHatayn
-                "N": u"\u064C", # Dammatayn
-                "K": u"\u064D", # kasratayn
-                "a": u"\u064E", # fatHa
-                "u": u"\u064F", # Damma
-                "i": u"\u0650", # kasra
-                "~": u"\u0651", # shaddah
-                "o": u"\u0652", # sukuun
-                "`": u"\u0670", # dagger 'alif
-                "{": u"\u0671", # waSla
+BUCKWALTER2UNICODE = {u"'": u"\u0621", # hamza-on-the-line
+                u"|": u"\u0622", # madda
+                u">": u"\u0623", # hamza-on-'alif
+                u"&": u"\u0624", # hamza-on-waaw
+                u"<": u"\u0625", # hamza-under-'alif
+                u"}": u"\u0626", # hamza-on-yaa'
+                u"A": u"\u0627", # bare 'alif
+                u"b": u"\u0628", # baa'
+                u"p": u"\u0629", # taa' marbuuTa
+                u"t": u"\u062A", # taa'
+                u"v": u"\u062B", # thaa'
+                u"j": u"\u062C", # jiim
+                u"H": u"\u062D", # Haa'
+                u"x": u"\u062E", # khaa'
+                u"d": u"\u062F", # daal
+                u"*": u"\u0630", # dhaal
+                u"r": u"\u0631", # raa'
+                u"z": u"\u0632", # zaay
+                u"s": u"\u0633", # siin
+                u"$": u"\u0634", # shiin
+                u"S": u"\u0635", # Saad
+                u"D": u"\u0636", # Daad
+                u"T": u"\u0637", # Taa'
+                u"Z": u"\u0638", # Zaa' (DHaa')
+                u"E": u"\u0639", # cayn
+                u"g": u"\u063A", # ghayn
+                u"_": u"\u0640", # taTwiil
+                u"f": u"\u0641", # faa'
+                u"q": u"\u0642", # qaaf
+                u"k": u"\u0643", # kaaf
+                u"l": u"\u0644", # laam
+                u"m": u"\u0645", # miim
+                u"n": u"\u0646", # nuun
+                u"h": u"\u0647", # haa'
+                u"w": u"\u0648", # waaw
+                u"Y": u"\u0649", # 'alif maqSuura
+                u"y": u"\u064A", # yaa'
+                u"F": u"\u064B", # fatHatayn
+                u"N": u"\u064C", # Dammatayn
+                u"K": u"\u064D", # kasratayn
+                u"a": u"\u064E", # fatHa
+                u"u": u"\u064F", # Damma
+                u"i": u"\u0650", # kasra
+                u"~": u"\u0651", # shaddah
+                u"o": u"\u0652", # sukuun
+                u"`": u"\u0670", # dagger 'alif
+                u"{": u"\u0671", # waSla
                 #extended here
-                "^": u"\u0653", # Maddah
-                "#": u"\u0654", # HamzaAbove
+                u"^": u"\u0653", # Maddah
+                u"#": u"\u0654", # HamzaAbove
 
-                ":"  : "\u06DC", # SmallHighSeen
-                "@"  : "\u06DF", # SmallHighRoundedZero
-                "\"" : "\u06E0", # SmallHighUprightRectangularZero
-                "["  : "\u06E2", # SmallHighMeemIsolatedForm
-                ";"  : "\u06E3", # SmallLowSeen
-                ","  : "\u06E5", # SmallWaw
-                "."  : "\u06E6", # SmallYa
-                "!"  : "\u06E8", # SmallHighNoon
-                "-"  : "\u06EA", # EmptyCentreLowStop
-                "+"  : "\u06EB", # EmptyCentreHighStop
-                "%"  : "\u06EC", # RoundedHighStopWithFilledCentre
-                "]"  : "\u06ED"          #
+                u":"  : u"\u06DC", # SmallHighSeen
+                u"@"  : u"\u06DF", # SmallHighRoundedZero
+                u"\"" : u"\u06E0", # SmallHighUprightRectangularZero
+                u"["  : u"\u06E2", # SmallHighMeemIsolatedForm
+                u";"  : u"\u06E3", # SmallLowSeen
+                u","  : u"\u06E5", # SmallWaw
+                u"."  : u"\u06E6", # SmallYa
+                u"!"  : u"\u06E8", # SmallHighNoon
+                u"-"  : u"\u06EA", # EmptyCentreLowStop
+                u"+"  : u"\u06EB", # EmptyCentreHighStop
+                u"%"  : u"\u06EC", # RoundedHighStopWithFilledCentre
+                u"]"  : u"\u06ED"          #
 
                 }
 
 # ISO233-2 romanization letter mapping
-ISO2UNICODE = { "ˌ": u"\u0621", # hamza-on-the-line
-                #"|": u"\u0622", # madda
-                "ˈ": u"\u0623", # hamza-on-'alif
-                "ˈ": u"\u0624", # hamza-on-waaw
-                #"<": u"\u0625", # hamza-under-'alif
-                "ˈ": u"\u0626", # hamza-on-yaa'
-                "ʾ": u"\u0627", # bare 'alif
-                "b": u"\u0628", # baa'
-                "ẗ": u"\u0629", # taa' marbuuTa
-                "t": u"\u062A", # taa'
-                "ṯ": u"\u062B", # thaa'
-                "ǧ": u"\u062C", # jiim
-                "ḥ": u"\u062D", # Haa'
-                "ẖ": u"\u062E", # khaa'
-                "d": u"\u062F", # daal
-                "ḏ": u"\u0630", # dhaal
-                "r": u"\u0631", # raa'
-                "z": u"\u0632", # zaay
-                "s": u"\u0633", # siin
-                "š": u"\u0634", # shiin
-                "ṣ": u"\u0635", # Saad
-                "ḍ": u"\u0636", # Daad
-                "ṭ": u"\u0637", # Taa'
-                "ẓ": u"\u0638", # Zaa' (DHaa')
-                "ʿ": u"\u0639", # cayn
-                "ġ": u"\u063A", # ghayn
-                #"_": u"\u0640", # taTwiil
-                "f": u"\u0641", # faa'
-                "q": u"\u0642", # qaaf
-                "k": u"\u0643", # kaaf
-                "l": u"\u0644", # laam
-                "m": u"\u0645", # miim
-                "n": u"\u0646", # nuun
-                "h": u"\u0647", # haa'
-                "w": u"\u0648", # waaw
-                "ỳ": u"\u0649", # 'alif maqSuura
-                "y": u"\u064A", # yaa'
-                "á": u"\u064B", # fatHatayn
-                "ú": u"\u064C", # Dammatayn
-                "í": u"\u064D", # kasratayn
-                "a": u"\u064E", # fatHa
-                "u": u"\u064F", # Damma
-                "i": u"\u0650", # kasra
-                #"~": u"\u0651", # shaddah
-                "°": u"\u0652", # sukuun
-                #"`": u"\u0670", # dagger 'alif
-                #"{": u"\u0671", # waSla
+ISO2UNICODE = { u"ˌ": u"\u0621", # hamza-on-the-line
+                #u"|": u"\u0622", # madda
+                u"ˈ": u"\u0623", # hamza-on-'alif
+                u"ˈ": u"\u0624", # hamza-on-waaw
+                #u"<": u"\u0625", # hamza-under-'alif
+                u"ˈ": u"\u0626", # hamza-on-yaa'
+                u"ʾ": u"\u0627", # bare 'alif
+                u"b": u"\u0628", # baa'
+                u"ẗ": u"\u0629", # taa' marbuuTa
+                u"t": u"\u062A", # taa'
+                u"ṯ": u"\u062B", # thaa'
+                u"ǧ": u"\u062C", # jiim
+                u"ḥ": u"\u062D", # Haa'
+                u"ẖ": u"\u062E", # khaa'
+                u"d": u"\u062F", # daal
+                u"ḏ": u"\u0630", # dhaal
+                u"r": u"\u0631", # raa'
+                u"z": u"\u0632", # zaay
+                u"s": u"\u0633", # siin
+                u"š": u"\u0634", # shiin
+                u"ṣ": u"\u0635", # Saad
+                u"ḍ": u"\u0636", # Daad
+                u"ṭ": u"\u0637", # Taa'
+                u"ẓ": u"\u0638", # Zaa' (DHaa')
+                u"ʿ": u"\u0639", # cayn
+                u"ġ": u"\u063A", # ghayn
+                #u"_": u"\u0640", # taTwiil
+                u"f": u"\u0641", # faa'
+                u"q": u"\u0642", # qaaf
+                u"k": u"\u0643", # kaaf
+                u"l": u"\u0644", # laam
+                u"m": u"\u0645", # miim
+                u"n": u"\u0646", # nuun
+                u"h": u"\u0647", # haa'
+                u"w": u"\u0648", # waaw
+                u"ỳ": u"\u0649", # 'alif maqSuura
+                u"y": u"\u064A", # yaa'
+                u"á": u"\u064B", # fatHatayn
+                u"ú": u"\u064C", # Dammatayn
+                u"í": u"\u064D", # kasratayn
+                u"a": u"\u064E", # fatHa
+                u"u": u"\u064F", # Damma
+                u"i": u"\u0650", # kasra
+                #u"~": u"\u0651", # shaddah
+                u"°": u"\u0652", # sukuun
+                #u"`": u"\u0670", # dagger 'alif
+                #u"{": u"\u0671", # waSla
                 ##extended here
-                #"^": u"\u0653", # Maddah
-                #"#": u"\u0654", # HamzaAbove
+                #u"^": u"\u0653", # Maddah
+                #u"#": u"\u0654", # HamzaAbove
 
-                #":"  : "\u06DC", # SmallHighSeen
-                #"@"  : "\u06DF", # SmallHighRoundedZero
-                #"\"" : "\u06E0", # SmallHighUprightRectangularZero
-                #"["  : "\u06E2", # SmallHighMeemIsolatedForm
-                #";"  : "\u06E3", # SmallLowSeen
-                #","  : "\u06E5", # SmallWaw
-                #"."  : "\u06E6", # SmallYa
-                #"!"  : "\u06E8", # SmallHighNoon
-                #"-"  : "\u06EA", # EmptyCentreLowStop
-                #"+"  : "\u06EB", # EmptyCentreHighStop
-                #"%"  : "\u06EC", # RoundedHighStopWithFilledCentre
-                #"]"  : "\u06ED"          #
+                #u":"  : "\u06DC", # SmallHighSeen
+                #u"@"  : "\u06DF", # SmallHighRoundedZero
+                #u"\"" : "\u06E0", # SmallHighUprightRectangularZero
+                #u"["  : "\u06E2", # SmallHighMeemIsolatedForm
+                #u";"  : "\u06E3", # SmallLowSeen
+                #u","  : "\u06E5", # SmallWaw
+                #u"."  : "\u06E6", # SmallYa
+                #u"!"  : "\u06E8", # SmallHighNoon
+                #u"-"  : "\u06EA", # EmptyCentreLowStop
+                #u"+"  : "\u06EB", # EmptyCentreHighStop
+                #u"%"  : "\u06EC", # RoundedHighStopWithFilledCentre
+                #u"]"  : "\u06ED"          #
 
                 }
 
@@ -165,7 +165,7 @@ def guess_romanization_system():
 	pass
 
 
-def transliterate( mode, string, ignore = "" , reverse = False ):
+def transliterate( mode, string, ignore = u"" , reverse = False ):
 	""" encode & decode different  romanization systems """
 
 	if ROMANIZATION_SYSTEMS_MAPPINGS.has_key( mode ):
@@ -184,7 +184,7 @@ def transliterate( mode, string, ignore = "" , reverse = False ):
 	result = ""
 	for char in string :
 		if mapping.has_key( char ) and char not in ignore:
-			result += mapping[char]
+			result += mapping[ char ] #test
 		else :
 			result += char
 	return result
