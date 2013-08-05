387d9a02b5e7054b6898eb64c192c65407d7de9c
Assem Chelli <assem.ch@gmail.com>
2013-08-05 15:10:32 +0100

    Implement the root website share button and updating the localization

--- src/alfanous-django/wui/static/shared/js/share_functions.js
+++ src/alfanous-django/wui/static/shared/js/share_functions.js
@@ -20,3 +20,27 @@
     enableTracking: false
     });
 
+
+    $('#shareme_root').sharrre({
+    	share: {
+    	googlePlus: true,
+    	facebook: true,
+    	twitter: true,
+    	digg: true,
+    	delicious: true
+    	},
+    	enableTracking: true,
+    	buttons: {
+    	googlePlus: {size: 'tall', annotation:'bubble'},
+    	facebook: {layout: 'box_count'},
+    	twitter: {count: 'vertical'},
+    	digg: {type: 'DiggMedium'},
+    	delicious: {size: 'tall'}
+    	},
+    	hover: function(api, options){
+    	$(api.element).find('.buttons').show();
+    	},
+    	hide: function(api, options){
+    	$(api.element).find('.buttons').hide();
+    	}
+    	});
