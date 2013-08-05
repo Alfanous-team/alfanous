652896319b2cbfb559c009744b297ef767ce3116
Assem Chelli <assem.ch@gmail.com>
2013-08-05 13:33:59 +0100

    replace via @_julian by via @alfanous, fix #260

--- src/alfanous-django/wui/static/shared/js/share_functions.js
+++ src/alfanous-django/wui/static/shared/js/share_functions.js
@@ -9,7 +9,7 @@
     buttons: {
     googlePlus: {size: 'tall', annotation:'bubble'},
     facebook: {layout: 'box_count'},
-    twitter: {count: 'vertical', via: '_JulienH'}
+    twitter: {count: 'vertical', via: 'alfanous'}
     },
     hover: function(api, options){
     $(api.element).find('.buttons').show();
