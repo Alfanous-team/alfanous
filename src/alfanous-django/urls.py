c694da4dabb64db2ba77d30396e71e837248f24d
Assem Chelli <assem.ch@gmail.com>
2013-11-26 13:06:57 +0100

    migrate to Django 1.6

--- src/alfanous-django/urls.py
+++ src/alfanous-django/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,12 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns( '',
     # Examples:
-    # url(r'^$', 'alfanousDjango.views.home', name='home'),
-    url( r'^jos2', 'alfanous-django.wui.views.jos2' ),
+    # url(r'^$', 'views.home', name='home'),
+    url( r'^jos2', 'wui.views.jos2' ),
     url( r'^admin/', include( admin.site.urls ) ),
-    url( r'^(?P<language>\w{2})/(?P<unit>\w{3,15})/', 'alfanous-django.wui.views.results' ),
-    url( r'^(?P<language>\w{2})/', 'alfanous-django.wui.views.results' ),
-    url( r'^(?P<unit>\w{3,15})/(?P<language>\w{2})/', 'alfanous-django.wui.views.results' ),
-    url( r'^(?P<unit>\w{3,15})/', 'alfanous-django.wui.views.results' ),
-    url( r'^', 'alfanous-django.wui.views.results' ),
+    url( r'^(?P<language>\w{2})/(?P<unit>\w{3,15})/', 'wui.views.results' ),
+    url( r'^(?P<language>\w{2})/', 'wui.views.results' ),
+    url( r'^(?P<unit>\w{3,15})/(?P<language>\w{2})/', 'wui.views.results' ),
+    url( r'^(?P<unit>\w{3,15})/', 'wui.views.results' ),
+    url( r'^', 'wui.views.results' ),
  )
