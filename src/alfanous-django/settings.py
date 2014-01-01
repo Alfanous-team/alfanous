c8180adb019e35e5a043931e8b7f7aa8e7140cdc
Mouad Debbar <mouad.debbar@gmail.com>
2014-01-01 12:56:58 -0800

    Change a few things in settings:     - Added myself as admin.     - Add ALLOWED_HOSTS which is required in production.     - Modified the logging config so that Django doesn't send error emails in DEBUG mode.     - Fixed indentation

--- src/alfanous-django/settings.py
+++ src/alfanous-django/settings.py
@@ -16,7 +16,7 @@ from ConfigParser import RawConfigParser
 configFile = "./settings.ini.proto" # e,g. '/etc/whatever/settings.ini'
 
 if configFile[:2] == "./":
-    print "WARNING: You need to specify a reliable absolute path to the config file, see settings.py"
+  print "WARNING: You need to specify a reliable absolute path to the config file, see settings.py"
 
 
 config = RawConfigParser()
@@ -53,21 +53,26 @@ DEBUG = MY_DEBUG
 TEMPLATE_DEBUG = MY_TEMPLATE_DEBUG
 
 ADMINS = (
-     ( 'Assem Chelli', 'assem.ch@gmail.com' ),
- )
+  ( 'Assem Chelli', 'assem.ch@gmail.com' ),
+  ( 'Mouad Debbar', 'mouad.debbar@gmail.com' ),
+)
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': DATABASE_ENGINE,      # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': DATABASE_NAME,          # Or path to database file if using sqlite3.
-        'USER': DATABASE_USER,          # Not used with sqlite3.
-        'PASSWORD': DATABASE_PASSWORD,  # Not used with sqlite3.
-        'HOST': DATABASE_HOST,          # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': DATABASE_PORT,          # Set to empty string for default. Not used with sqlite3.
-    }
+  'default': {
+    'ENGINE': DATABASE_ENGINE,      # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': DATABASE_NAME,          # Or path to database file if using sqlite3.
+    'USER': DATABASE_USER,          # Not used with sqlite3.
+    'PASSWORD': DATABASE_PASSWORD,  # Not used with sqlite3.
+    'HOST': DATABASE_HOST,          # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': DATABASE_PORT,          # Set to empty string for default. Not used with sqlite3.
+  }
 }
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -83,20 +88,20 @@ TIME_ZONE = 'Africa/Algiers'
 LANGUAGE_CODE = 'en'
 
 LANGUAGES = (
-    ( 'ar', "Arabic" ),
-    ( 'en', "English" ),
-    ( 'fr', "French" ),
-    ( 'id', "Indonesian" ),
-    ( 'ja', "Japanese"),
-    ( 'ku', "Kurdish" ),
-    #( 'ur', "Urdu" ),
-    ( 'ms', "Malay" ),
-    ( 'ml', "Malayalam" ),
-    #( 'tr', "Turkish" ),
-    ( 'es', "Spanish" ),
-    ( 'pt', "Portuguese"),
-    #( 'sv', "swedish" )
- )
+  ( 'ar', "Arabic" ),
+  ( 'en', "English" ),
+  ( 'fr', "French" ),
+  ( 'id', "Indonesian" ),
+  ( 'ja', "Japanese"),
+  ( 'ku', "Kurdish" ),
+  #( 'ur', "Urdu" ),
+  ( 'ms', "Malay" ),
+  ( 'ml', "Malayalam" ),
+  #( 'tr', "Turkish" ),
+  ( 'es', "Spanish" ),
+  ( 'pt', "Portuguese"),
+  #( 'sv', "swedish" )
+)
 
 
 SITE_ID = 1
@@ -135,18 +140,18 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
- )
+  # Put strings here, like "/home/html/static" or "C:/www/django/static".
+  # Always use forward slashes, even on Windows.
+  # Don't forget to use absolute paths, not relative paths.
+)
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
- )
+  'django.contrib.staticfiles.finders.FileSystemFinder',
+  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+  # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = MY_SECRET_KEY
@@ -154,27 +159,27 @@ SECRET_KEY = MY_SECRET_KEY
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
- )
+  'django.template.loaders.filesystem.Loader',
+  'django.template.loaders.app_directories.Loader',
+  # 'django.template.loaders.eggs.Loader',
+)
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
- )
+  'django.middleware.common.CommonMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+)
 
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    MY_TEMPLATE_DIR,
- )
+  # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+  # Always use forward slashes, even on Windows.
+  # Don't forget to use absolute paths, not relative paths.
+  MY_TEMPLATE_DIR,
+)
 
 INSTALLED_APPS = (
   # Django apps
@@ -192,7 +197,7 @@ INSTALLED_APPS = (
 
   # our apps
   'wui',
- )
+)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
@@ -200,20 +205,26 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
+  'version': 1,
+  'disable_existing_loggers': False,
+  'filters': {
+    'require_debug_false': {
+      '()': 'django.utils.log.RequireDebugFalse'
+    }
+  },
+  'handlers': {
+    'mail_admins': {
+      'level': 'ERROR',
+      'filters': ['require_debug_false'],
+      'class': 'django.utils.log.AdminEmailHandler'
+    }
+  },
+  'loggers': {
+    'django.request': {
+      'handlers': ['mail_admins'],
+      'level': 'ERROR',
+      'propagate': True,
+      },
     }
 }
 
@@ -221,5 +232,3 @@ LOGGING = {
 LOCALE_PATHS = (
   os.path.join(ROOT_DIR, MY_LOCALE_PATH),
 )
-
-
