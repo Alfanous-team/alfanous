5d1afc9fbed960c1c9cde529972986e1ac5f02dc
Assem Chelli <assem.ch@gmail.com>
2018-10-13 02:14:40 +0100

    fix allowed hosts

--- src/alfanous-django/settings.py
+++ src/alfanous-django/settings.py
@@ -11,7 +11,7 @@ ROOT_DIR = os.path.dirname(__file__)
 #     Static and Public settings       #
 ########################################
 
-DEBUG = True
+DEBUG = False
 
 INSTALLED_APPS = (
   # Django apps
@@ -53,7 +53,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.afalnous.org']
+ALLOWED_HOSTS = ['127.0.0.1', '.alfanous.org']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
