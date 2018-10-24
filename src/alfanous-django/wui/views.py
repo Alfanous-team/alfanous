fadefd8496a3aa714a00f40d4e3f12ff4b714bdd
Assem Chelli <assem.ch@gmail.com>
2018-10-24 10:22:12 +0100

    check if query exists to api calls

--- src/alfanous-django/wui/views.py
+++ src/alfanous-django/wui/views.py
@@ -44,7 +44,7 @@ def api(request,action='search'):
         return HttpResponse('action should be one of: {}'.format(','.join(RAWoutput._domains['action'])), status=status.HTTP_400_BAD_REQUEST)
 
     control_access(request)
-    if len(request.GET):
+    if request.GET.get('query'):
         response_data = RAWoutput.do(request.GET)
         response = HttpResponse(
             json.dumps(response_data, sort_keys=False),
@@ -55,12 +55,10 @@ def api(request,action='search'):
         response['Access-Control-Allow-Methods'] = 'GET'
         # response['Content-Encoding'] = 'gzip'
     else:
-        response_html = "<html><head> <title> %(title)s </title> </head><body> %(body)s </body> </html>"
-        response_data = response_html % {
-            "title": "Alfanous API",
-            "body": RAWoutput._information["json_output_system_note"]
-        }
-        response = HttpResponse(response_data)
+        response = HttpResponse(
+            json.dumps({}, sort_keys=False, indent=4),
+            content_type="application/json"
+        )
     return response
 
 @gzip_page
