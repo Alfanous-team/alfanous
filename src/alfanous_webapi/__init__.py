b62825ccee36dfbdc14492de57c485021f0ca405
Assem Ch <assem.ch@gmail.com>
2026-02-17 00:50:17 +0100

    Update __init__.py

--- src/alfanous_webapi/__init__.py
+++ src/alfanous_webapi/__init__.py
@@ -8,8 +8,8 @@ A FastAPI web application that exposes the Alfanous Quranic search API
 through RESTful endpoints.
 """
 
-__version__ = "0.7.33"
-__author__ = "Assem Chelli"
+__version__ = "0.1"
+__author__ = "Assem"
 
 from alfanous_webapi.web_api import app, main
 
